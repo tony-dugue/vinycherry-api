902c8f9de09c0546fd72353722f5dc3d24066853
Tony Dugué <duguetony@gmail.com>
2023-08-12 22:21:05 +0200

    feat(auth): mise en place de la logique du register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  register() {
-    return { msg: 'Mon compte a été crée!' };
+  async register(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException("Un utilisateur possède déjà cet email!");
+        }
+      }
+      throw error;
+    }
   }
 
   login() {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  // on instancie l'application
   const app = await NestFactory.create(AppModule);
+
+  // activation globalement des pipes de validation dans l'app
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+  );
+
+  // on exécute le serveur sur le port 8000
   await app.listen(8000);
 }
 bootstrap();
