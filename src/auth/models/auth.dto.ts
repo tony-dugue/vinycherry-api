6eb9a53fa78e694e5301500a238299eb482742ee
Tony Dugué <duguetony@gmail.com>
2023-09-03 10:57:54 +0200

    feat(auth): optimization ApiProperty to DTOs

--- src/auth/models/auth.dto.ts
+++ src/auth/models/auth.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty({ message: 'Votre email est obligatoire!' })
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty({ message: 'Votre mot de passe est obligatoire!' })
   password: string;
--- src/auth/models/register-user.dto.ts
+++ src/auth/models/register-user.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterUserDto {
+  @ApiProperty()
   @IsEmail({}, { message: "Le format email n'est pas respecté!" })
   @IsNotEmpty({ message: 'Un email est obligatoire!' })
   email: string;
 
+  @ApiProperty()
   @IsString({ message: 'la propriété password doit être de type String!' })
   @IsNotEmpty({ message: 'Un mot de passe est obligatoire!' })
   password: string;
 
+  @ApiProperty()
   @IsString({ message: 'la propriété confirmationPassword doit être de type String!' })
   @IsNotEmpty({ message: 'Un mot de passe de confirmation est obligatoire!' })
   confirmationPassword: string;
 
+  @ApiProperty()
   @IsString({ message: 'la propriété firstName doit être de type String!' })
   @IsNotEmpty({ message: 'Un prénom est obligatoire!' })
   firstName: string;
 
+  @ApiProperty()
   @IsString({ message: 'la propriété lastName doit être de type String!' })
   @IsNotEmpty({ message: 'Un nom est obligatoire!' })
   lastName: string;
